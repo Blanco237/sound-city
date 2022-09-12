9a404ebfd3733ce6b23ac6a93e7fc5b96bc9e028
Blanco237 <asongrandy9@gmail.com>
2022-09-12 13:34:14 +0100

    Completed Header

---
+++ client/src/components/shared/header/Header.jsx
@@ -0,0 +1,41 @@
+import React, { useState } from "react";
+
+import logo from "../../../assets/logo.png";
+
+import { Turn as Hamburger } from "hamburger-react";
+
+const Header = () => {
+  const [isOpen, setOpen] = useState(false);
+
+  return (
+    <header className="w-full bg-secondary flex px-6 py-2 justify-between text-white fixed">
+      <div className="md:w-1/6 flex items-center gap-2 text-xl font-medium">
+        <img
+          src={logo}
+          alt="Souncity logo"
+          className="md:w-[15%] w-1/12 drop-shadow-lg shadow-primary"
+        />
+        Sound City
+      </div>
+      <nav className="flex">
+        <ul className={`flex items-center md:h-full gap-5 md:static absolute transition-all duration-500  left-0 py-5 w-screen md:w-fit md:flex-row flex-col bg-secondary ${isOpen? 'top-16' : 'top-[-99rem]'}`}>
+          {["Home", "Explore", "Login", "Register"].map((item, index) => {
+            return ["Login", "Register"].includes(item) ? (
+              <button
+                key={item}
+                className="px-5 py-1 bg-primary rounded-2xl shadow-none hover:shadow-glow hover:shadow-primary transition-shadow duration-500"
+              >
+                {item}
+              </button>
+            ) : (
+              <li key={index + item} className='w-full md:w-fit text-center '>{item}</li>
+            );
+          })}
+        </ul>
+        <div className="md:hidden"><Hamburger toggled={isOpen} toggle={setOpen} rounded /></div>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -5,11 +5,19 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        'cyan' : '#3bc8e7',
+        'primary' : '#3bc8e7',
         'gray' : '#777777',
-        'deep-blue' : '#14182a',
+        'secondary' : '#14182a',
+      },
+      backgroundImage : {
+        'fade' : 'radial-gradient(50% 50%, ellipse closest-side, #fff, rgba(255, 42, 112, 0) 60%)'
+      },
+      boxShadow : {
+        'glow' : '0 0px 25px -1px var(--tw-shadow-color), 0 0px 4px -2px var(--tw-shadow-color)'
       }
     },
   },
-  plugins: [],
+  plugins: [
+    require("tailwindcss-animate"),
+  ],
 }
