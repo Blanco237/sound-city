c562e32fee3d37d33889a84143ba7d1e08e6a977
Blanco237 <asongrandy9@gmail.com>
2022-09-14 00:08:04 +0100

    Did some stuff

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,30 @@
 import './App.css';
+
+import { UserProvider } from './Context/UserContext';
+
+
+import Notifications from 'react-notify-toast'
+
 import Footer from './components/shared/footer/Footer';
 import Header from './components/shared/header/Header';
 import Home from './pages/home/Home';
 import Upload from './pages/upload/Upload';
+import Loader from './components/shared/Loader/Loader';
+import { LoadingProvider } from './Context/LoadingContext';
 
 function App() {
   return (
     <>
-      <Header />
-      {/* <Home /> */}
-      <Upload />
-      <Footer />
+      <UserProvider >
+        <LoadingProvider>
+          <Notifications />
+          <Header />
+          {/* <Home /> */}
+          <Upload />
+          <Footer />
+          <Loader />
+        </LoadingProvider>
+      </UserProvider>
     </>
   );
 }
