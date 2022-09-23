4fd5d01e074a81671cc4838aa643068f62349ccb
Blanco237 <asongrandy9@gmail.com>
2022-09-23 23:30:48 +0100

    Setup routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,18 @@
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
-import { UserProvider } from './Context/UserContext';
-
-
-import Notifications from 'react-notify-toast'
-
+//Component Imports
 import Footer from './components/shared/footer/Footer';
 import Header from './components/shared/header/Header';
 import Home from './pages/home/Home';
 import Upload from './pages/upload/Upload';
 import Loader from './components/shared/Loader/Loader';
-import { LoadingProvider } from './Context/LoadingContext';
 import Library from './pages/library/Library';
+
+//Providers
+import { UserProvider } from './Context/UserContext';
+import Notifications from 'react-notify-toast'
+import { LoadingProvider } from './Context/LoadingContext';
+import { ROUTES } from './utils/statics';
 
 function App() {
   return (
@@ -20,9 +21,14 @@ function App() {
         <LoadingProvider>
           <Notifications />
           <Header />
-          {/* <Home /> */}
-          {/* <Upload /> */}
-          <Library />
+          <Router>
+            <Routes>
+              <Route path={ROUTES.HOME} element={<Home />} />
+              <Route path={ROUTES.LIBRARY} element={<Library />} />
+              <Route path={ROUTES.EXPLORE} element={<div className='w-screen h-screen grid place-items-center'> Explore Page, Coming soon</div>} />
+              <Route path={ROUTES.UPLOAD} element={<Upload />} />
+            </Routes>
+          </Router>
           <Footer />
           <Loader />
         </LoadingProvider>
