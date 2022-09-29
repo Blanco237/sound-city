0fb7c83fd4233a758869cef13d7ee17249300c2c
Blanco237 <asongrandy9@gmail.com>
2022-09-29 23:14:55 +0100

    Updates

--- server/index.js
+++ server/index.js
@@ -2,7 +2,10 @@ const express = require("express");
 const cors = require('cors');
 const fileUpload = require('express-fileupload');
 
+
 require('dotenv').config();
+
+
 
 const app = express();
 
@@ -24,10 +27,10 @@ const uploadRouter = require('./routes/Upload');
 app.use('/users', userRouter);
 app.use('/upload', uploadRouter);
 
-
 const port = process.env.PORT || 5000;
 db.sequelize.sync().then(() => {
     app.listen(port, () => {
         console.log(`Server is running on ${port}`)
     })
 })
+
--- server/routes/Upload.js
+++ server/routes/Upload.js
@@ -1,7 +1,7 @@
 const express = require('express');
-const cloudinary = require('cloudinary').v2;
-
 const router = express.Router();
+
+const cloudinary = require('cloudinary').v2;
 
 cloudinary.config({
     cloud_name: 'blanco237',
@@ -9,8 +9,27 @@ cloudinary.config({
     api_secret: 'QUP-PekAXZZOrd0Ffkwx5igunoI'
 });
 
+router.post('/image', async (req, res) => {
+    console.log("Image Upload Request");
+    const file = req.files.image;
+    if(!file){
+        console.log(req);
+        return;
+    }
 
-router.post('/audio', (req, res) => {
+    const result = await cloudinary.uploader.upload(file.tempFilePath, {
+        folder: "image",
+        resource_type: "image"
+    })
+
+    res.json(result);
+});
+
+
+router.post('/audio', async (req, res) => {
+
+    console.log("Audio Upload")
+
     const file = req.files.audio;
     if (!file) {
         console.log(req);
@@ -24,20 +43,9 @@ router.post('/audio', (req, res) => {
     res.json(result);
 })
 
-router.post('/image', (req, res) => {
-    const file = req.files.image;
-    if(!file){
-        console.log(req);
-        return;
-    }
 
-    const result = await cloudinary.uploader.upload(file.tempFilePath, {
-        folder: "image",
-        resource_type: "image"
-    })
 
-    res.json(result);
-})
+
 
 
 module.exports = router;
