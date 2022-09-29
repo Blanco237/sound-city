b664c659ad4ac61cd876b089f49151d900dcf2e4
Blanco237 <asongrandy9@gmail.com>
2022-09-29 18:46:53 +0100

    Completed Music Player

--- client/src/pages/play/Play.jsx
+++ client/src/pages/play/Play.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { useParams } from 'react-router-dom'
+import OtherTitles from '../../components/Play/OtherTitles';
 import Player from '../../components/Play/Player'
 
 const Play = () => {
@@ -9,8 +10,9 @@ const Play = () => {
     console.log(id);
 
   return (
-    <div className='md:pt-[7rem] pt-[6rem] bg-secondary'>
+    <div className='md:pt-[7rem] pt-[6rem] pb-5 bg-secondary flex flex-col gap-10'>
         <Player />
+        <OtherTitles />
     </div>
   )
 }
