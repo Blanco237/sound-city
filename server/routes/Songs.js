5b53236eb44cb0e971217e0b39bb7b11bdd546cf
Blanco237 <asongrandy9@gmail.com>
2022-10-01 16:33:36 +0100

    Added more routes

--- server/routes/Songs.js
+++ server/routes/Songs.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { Op } = require('sequelize');
 const router = express.Router();
 
 const { Songs } = require('../models');
@@ -34,7 +35,7 @@ router.get('/getAll', async (req, res) => {
 router.get('/getLibrary/:uid', async (req, res) => {
     const uid = req.params.uid;
     try {
-        const mySongs = await Songs.findAll({
+        const mySongs = await Songs.findAndCountAll({
             where: {
                 uid
             }
@@ -61,4 +62,42 @@ router.post('/updateCount', async (req, res) => {
     await Songs.increment({playCount: 1}, {where: {sid}})
 })
 
+router.get('/uploadCount/:uid', async (req, res) => {
+    const { uid } = req.params;
+    try {
+        const uploadCount = await Songs.count({
+            where: {
+                uid
+            }
+        })
+        res.json(uploadCount);
+    }catch(e){
+        res.status(400).json({error: e.message});
+    }
+})
+
+router.get('/similar/:sid', async (req, res) => {
+    const { sid } = req.params;
+
+    try{
+        const song = await Songs.findByPk(sid);
+        const similarSongs = await Songs.findAll({
+            where: {
+                [Op.or] : [
+                    { artist: song.artist },
+                    { album: song.album},
+                    { uid: song.uid}
+                ]
+            },
+            limit: 10,
+            order : [
+                ['playCount', 'ASC']
+            ]
+        })
+        res.json(similarSongs);
+    }
+    catch(e){
+        res.status(400).json({error: e.message});
+    }
+})
 module.exports = router;
